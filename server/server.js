767d7d0c36d59e3bb48b243e268d08ca0801a2ba
Satish Ramdas Chawali <148488004+satishchawali@users.noreply.github.com>
2025-04-06 17:33:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://authentication-system-frontend-juty.onrender.com']
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}));
