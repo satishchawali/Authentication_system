179f4030db10eee0ce45713e82c70fc4fb596b12
Satish Chawali <satishchawali40@gmail.com>
2025-04-13 10:17:25 +0530

    improvements

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,17 @@
 import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
 
     const {userData} = useContext(AppContext)
+    const navigate = useNavigate()
+
+    const navToLogin = (e) =>{
+      e.preventDefault()
+      navigate('/login')
+    }
 
   return (
     <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
@@ -18,7 +25,7 @@ const Header = () => {
 
         <p className='mb-8 max-w-md'>Let's start with a quick product tour and we will have you up and running in no time!</p>
 
-        <button className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>Get Started</button>
+        <button onClick={navToLogin} className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>Get Started</button>
     </div>
   )
 }
