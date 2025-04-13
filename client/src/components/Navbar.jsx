5761691eeb1e7c499ec86cee4498e875d1f22d3e
Satish Chawali <satishchawali40@gmail.com>
2025-04-13 10:30:22 +0530

    user icon click feature added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,58 +4,66 @@ import { useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { useState } from 'react'
 
 const Navbar = () => {
 
-    const navigate = useNavigate()
-    const {userData, backendUrl, setUserData, setIsLoggedin} = useContext(AppContext)
+  const navigate = useNavigate()
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const sendVerificationOtp = async ()=>{
+  const toggleMenu = () => setIsMenuOpen(prev => !prev);
+  const closeMenu = () => setIsMenuOpen(false);
+  const { userData, backendUrl, setUserData, setIsLoggedin } = useContext(AppContext)
 
-      try {
-        axios.defaults.withCredentials = true;
+  const sendVerificationOtp = async () => {
 
-        const {data}= await axios.post(backendUrl + '/api/auth/send-verify-otp')
-        
-        if(data.success){
-          navigate('/email-verify')
-          toast.success(data.message)
-        }
-        else{
-          toast.error(data.message)
-        }
+    try {
+      axios.defaults.withCredentials = true;
 
-      } catch (error) {
-        toast.error(error.message)
+      const { data } = await axios.post(backendUrl + '/api/auth/send-verify-otp')
+
+      if (data.success) {
+        navigate('/email-verify')
+        toast.success(data.message)
       }
-    }
-
-    const logout = async ()=>{
-
-      try {
-        axios.defaults.withCredentials = true
-        const { data } = await axios.post(backendUrl + '/api/auth/logout')
-        data.success && setIsLoggedin(false)
-        data.success && setUserData(false)
-        navigate('/')
-      } catch (error) {
-        toast.error(error.message)
+      else {
+        toast.error(data.message)
       }
+
+    } catch (error) {
+      toast.error(error.message)
     }
+  }
+
+  const logout = async () => {
+
+    try {
+      axios.defaults.withCredentials = true
+      const { data } = await axios.post(backendUrl + '/api/auth/logout')
+      data.success && setIsLoggedin(false)
+      data.success && setUserData(false)
+      navigate('/')
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
 
   return (
     <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
-        
-        <img src={assets.logo} alt="" className='w-28 sm:w-32'/>
-        {userData ?
-        <div className='w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group'>
+
+      <img src={assets.logo} alt="" className='w-28 sm:w-32' />
+      {userData ?
+        <div onClick={toggleMenu} onMouseLeave={closeMenu} className='w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group'>
           {userData.name[0].toUpperCase()}
-          <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
+          <div className={`absolute top-0 right-0 z-10 text-black rounded pt-10 ${
+          isMenuOpen ? 'block' : 'hidden group-hover:block'
+        }`}>
             <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
 
               {!userData.isAccountVerified && <li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify Email</li>}
 
-              
+
               <li onClick={logout} className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
 
             </ul>
@@ -63,9 +71,9 @@ const Navbar = () => {
         </div>
 
         :
-        <button onClick={()=>navigate('/login')} className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>Login <img src={assets.arrow_icon} alt="" /></button>
-          }
-        
+        <button onClick={() => navigate('/login')} className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>Login <img src={assets.arrow_icon} alt="" /></button>
+      }
+
     </div>
   )
 }
